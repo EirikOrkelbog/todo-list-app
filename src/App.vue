abd5766b86a81ed9c1639b1fd68578142d02f5c4
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-10-27 16:38:14 +0200

    added html for heading and adding sections

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
 </script>
 
 <template>
-  <div>
-  </div>
+  <main>
+    <section class="heading">
+      <h1>What's up <input type="text" placeholder="name here"></h1>
+    </section>
+
+    <section class="adding">
+      <h3>What's on your todo list?</h3>
+      <form action="submit">
+        <input type="text">
+        <button>Add todo</button>
+      </form>
+    </section>
+  </main>
 </template>
