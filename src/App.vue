b9390271f99a9251669c3298de1105ea6f721c7e
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-10-28 15:49:25 +0200

    created addTodo function

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { ref, onMounted, watch } from 'vue'
 
+let id = 0;
 const headingInput = ref('');
+const newTodo = ref('');
+const todos = ref([]);
 
 // Watches for changes in the headingInput variable and stores the value in local storage
 watch(headingInput, (newValue) => {
@@ -12,20 +15,33 @@ watch(headingInput, (newValue) => {
 onMounted(() => {
   headingInput.value = localStorage.getItem('headingInput') || ''
 })
+
+// Pushing an object with id and text to todos array + setting the input value back to an empty string 
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value })
+  newTodo.value = ''
+}
 </script>
 
 <template>
   <main>
     <section class="heading">
-      <h1>What's up <input type="text" placeholder="name here" v-model="headingInput"></h1>
+      <h1>What's up <input type="text" placeholder="name here" v-model="headingInput" /></h1>
     </section>
 
     <section class="adding">
       <h3>What's on your todo list?</h3>
-      <form action="submit">
-        <input type="text">
+      <form @submit.prevent="addTodo">
+        <input v-model="newTodo" />
         <button>Add todo</button>
       </form>
+    </section>
+
+    <section class="list">
+      <h3>Todo list</h3>
+      <ul>
+        <li v-for="todo in todos" :id="todo.id">{{ todo.text }}</li>
+      </ul>
     </section>
   </main>
 </template>
