0c29a668e5138126be185de91dd6d4a406613852
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-10-28 14:45:41 +0200

    saved heading input in local storage

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup>
+import { ref, onMounted, watch } from 'vue'
+
+const headingInput = ref('');
+
+// Watches for changes in the headingInput variable and stores the value in local storage
+watch(headingInput, (newValue) => {
+  localStorage.setItem('headingInput', newValue);
+})
+
+// Looks for value under the headingInput key and stores it in headingInput
+onMounted(() => {
+  headingInput.value = localStorage.getItem('headingInput') || ''
+})
 </script>
 
 <template>
   <main>
     <section class="heading">
-      <h1>What's up <input type="text" placeholder="name here"></h1>
+      <h1>What's up <input type="text" placeholder="name here" v-model="headingInput"></h1>
     </section>
 
     <section class="adding">
